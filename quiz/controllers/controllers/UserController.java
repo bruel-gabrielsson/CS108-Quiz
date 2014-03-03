19a65045b847a8811a23f8ef00d450cdc7903a88
Jikyu Choi <jikyu@bitcasa.com>
2014-03-03 09:26:51 -0800

    Renamed src dir to app
    Signed-off-by: Jikyu Choi <jikyu@bitcasa.com>

--- quiz/controllers/controllers/UserController.java
+++ quiz/controllers/controllers/UserController.java
@@ -30,32 +30,37 @@ public class UserController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Recieved GET request");
+		System.out.println("UserController: Recieved GET request");
 		
 		// Retrieve username & password from the request
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 
-		App app = (App)this.getServletContext().getAttribute("app");
+		// Get app
+		App app = (App)request.getSession().getAttribute("app");
 		if(app == null){
-			System.out.println("App not found!");
+			System.out.println("UserController: App not found!");
 			return;
 		}
 		
-		if(!username.isEmpty() && !password.isEmpty()){
-			
-			// Check for auth here
-			if (app.signIn(username, password)){
-				// Login successful,Store username as a session variable
+		// Begin auth procedures
+		if(username != null && !username.isEmpty() && password != null && !password.isEmpty()){
+			System.out.println("UserController: Trying to login user: " + username);
+
+			if (app.signIn(username, password)){				
+				System.out.println("UserController: Login success for user: " + username);
+				
+				// Login successful, Store username as a session variable
 				request.getSession().setAttribute("username", username);
 				request.getSession().setAttribute("loginStatus", "Success");
 			} else {
+				
+				// Login unsuccessful
 				request.getSession().setAttribute("loginStatus", "Failed to login");
 			}
 		} else {
 			request.getSession().setAttribute("loginStatus", "Invalid username/password");
 		}
-		
 		
 		RequestDispatcher dispatch = request.getRequestDispatcher("home.jsp");
 		dispatch.forward(request, response);	
